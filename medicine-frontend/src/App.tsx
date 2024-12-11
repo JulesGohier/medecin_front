dd2596861150741a0fd3b943b32da592506600d8
Lilian CAFFIER <lilian.caffier@bts-malraux.net>
2024-12-11 19:50:31 +0100

    Implementation of react-router :construction:

--- medicine-frontend/src/App.tsx
+++ medicine-frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import {Button} from "@/components/ui/button.tsx";
-import { Camera } from 'lucide-react';
+import {Route, Routes} from "react-router";
+import Homepage from "@/templates/Homepage.tsx";
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <Button onClick={() => {setCount(count + 1)}}>
-          <Camera /> {count + 1} testdedzd
-      </Button>
+      <Routes>
+        <Route path={"/"} element={<Homepage />} />
+      </Routes>
   )
 }
 
