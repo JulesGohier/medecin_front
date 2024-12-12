ae3c52ddb60bd5b196f1a2ab898b8038a6a6e925
JulesGohier <jules.reihog5@gmail.com>
2024-12-12 15:25:19 +0100

    init github action

--- medicine-frontend/src/App.tsx
+++ medicine-frontend/src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import {Route, Routes} from "react-router";
+import "./App.css";
+import { Route, Routes } from "react-router";
 import Homepage from "@/templates/Homepage.tsx";
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <Routes>
-        <Route path={"/"} element={<Homepage />} />
-      </Routes>
-  )
+    <Routes>
+      <Route path={"/"} element={<Homepage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- medicine-frontend/src/components/features/layout/SidebarComponent.tsx
+++ medicine-frontend/src/components/features/layout/SidebarComponent.tsx
@@ -1,59 +1,64 @@
 import SidebarLink from "@/components/features/layout/SidebarLink.tsx";
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar.tsx";
 import { LayoutDashboard } from "lucide-react";
 
 export const SidebarComponent = () => {
-    const items = [
-        {
-            title: "Tableau de bord",
-            url: "/",
-            icon: LayoutDashboard,
-        }
-    ]
-    
-    return (
-        <Sidebar>
-            <SidebarHeader>
-                <div className={"flex items-center py-2 px-2"}>
-                    <img
-                        width={75}
-                        height={75}
-                        src={"/icon-medicine.png"}
-                        alt={"Medicine Logo"}
-                    />
-                    <h2 className={"text-3xl ml-2 font-bold"}>
-                        <span className={"text-red-800"}>M</span>
-                        edicine
-                    </h2>
-                </div>
-            </SidebarHeader>
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel className={"uppercase text-[#929DAC] text-lg"}>Principal</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <SidebarLink
-                                            href={item.url}
-                                            icon={item.icon}
-                                            label={item.title}
-                                            />
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-        </Sidebar>
-    );
-};
+  const items = [
+    {
+      title: "Tableau de bord",
+      url: "/",
+      icon: LayoutDashboard,
+    },
+  ];
 
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <div className={"flex items-center py-2 px-2"}>
+          <img
+            width={75}
+            height={75}
+            src={"/icon-medicine.png"}
+            alt={"Medicine Logo"}
+          />
+          <h2 className={"text-3xl ml-2 font-bold"}>
+            <span className={"text-red-800"}>M</span>
+            edicine
+          </h2>
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className={"uppercase text-[#929DAC] text-lg"}>
+            Principal
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <SidebarLink
+                      href={item.url}
+                      icon={item.icon}
+                      label={item.title}
+                    />
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
