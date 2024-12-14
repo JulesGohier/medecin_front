cfba6e25a823c2381598917e7e6faf0767f60eb0
Thomas Kerherve <titom.kerherve@gmail.com>
2024-12-14 19:07:52 +0100

    En-Tete fin

--- medicine-frontend/src/templates/Homepage.tsx
+++ medicine-frontend/src/templates/Homepage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "@/assets/icon-medicine.png";
 import {SidebarLayout} from "@/components/features/layout/SidebarLayout.tsx";
 import {Button} from "@/components/ui/button.tsx";
 
@@ -6,10 +7,27 @@ import {Button} from "@/components/ui/button.tsx";
 const Homepage = () => {
     return (
       <div className="font-sans bg-gray-100">
-        {/* Header */}
-        <header className="flex items-center justify-between p-4 bg-grey shadow-md">
-        <img src="medecin_front/medicine-frontend/src/templates/logo.PNG" alt="Logo"/>
+        {/* En-Tete */}
+        <header className="flex items-center justify-between p-4 bg-grey shadow-lg">
+        <div className="flex items-center text-2xl font-bold text-red-600">
+            <img className="font-bold w-20 h-20" src={logo} alt="Logo"/>Médecine
+        </div>
+        
+          <nav className="flex gap-4">
+            <a href="#" className="text-gray-600 hover:text-black">Accueil</a>
+            <div className="text-gray-600 hover:text-black" >|</div>
+            <a href="#" className="text-gray-600 hover:text-black">Médecin</a>
+            <div className="text-gray-600 hover:text-black" >|</div>
+            <a href="#" className="text-gray-600 hover:text-black">Patient</a>
+            <div className="text-gray-600 hover:text-black" >|</div>
+            <a href="#" className="text-gray-600 hover:text-black">Notre Équipe</a>
+          </nav>
+          <div className="flex gap-2">
+            <button className="px-4 py-1 text-white bg-red-500 rounded-md">Connexion</button>
+            <button className="px-4 py-1 text-white bg-red-500 rounded-md">Inscription</button>
+          </div>
         </header>
+        
   
       </div>
     );
