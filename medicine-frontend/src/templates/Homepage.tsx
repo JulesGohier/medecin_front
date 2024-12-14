cf595a563eda23ff68a4a4b481502335542580f8
Thomas Kerherve <titom.kerherve@gmail.com>
2024-12-14 18:12:25 +0100

    Debut de l'En-Tete

--- medicine-frontend/src/templates/Homepage.tsx
+++ medicine-frontend/src/templates/Homepage.tsx
@@ -1,11 +1,18 @@
+import React from "react";
 import {SidebarLayout} from "@/components/features/layout/SidebarLayout.tsx";
 import {Button} from "@/components/ui/button.tsx";
 
 
 const Homepage = () => {
     return (
-        <h1></h1>>
+      <div className="font-sans bg-gray-100">
+        {/* Header */}
+        <header className="flex items-center justify-between p-4 bg-grey shadow-md">
+        <img src="medecin_front/medicine-frontend/src/templates/logo.PNG" alt="Logo"/>
+        </header>
+  
+      </div>
     );
-};
-
-export default Homepage;
+  };
+  
+  export default Homepage;
