4b4037a172feaabc4791b9ca9b362adad3a7546e
Lilian CAFFIER <lilian.caffier@bts-malraux.net>
2024-12-16 18:43:48 +0100

    Add PatientsAreaChart component :chart:

---
+++ medicine-frontend/src/medecin/components/charts/PatientsAreaChart.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart.tsx"
+
+const chartData = [
+    { month: "Janvier", patients: 186 },
+    { month: "Février", patients: 305 },
+    { month: "Mars", patients: 237 },
+    { month: "Avril", patients: 73 },
+    { month: "Mai", patients: 209 },
+    { month: "Juin", patients: 214 },
+    { month: "Juillet", patients: 412 },
+    { month: "Août", patients: 356 },
+    { month: "Septembre", patients: 125 },
+    { month: "Octobre", patients: 615 },
+    { month: "Novembre", patients: 25 },
+    { month: "Décembre", patients: 333 },
+]
+
+const chartConfig = {
+    desktop: {
+        label: "Patients",
+        color: "hsl(var(--chart-5))",
+    },
+} satisfies ChartConfig
+
+export const PatientsAreaChart = () => {
+    return (
+        <Card className="border-2">
+            <CardHeader>
+                <CardTitle>Statistiques Patients</CardTitle>
+                <CardDescription>Janvier - Décembre 2024</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig}>
+                    <BarChart
+                        accessibilityLayer
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                        }}
+                    >
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="month"
+                            tickLine={false}
+                            tickMargin={10}
+                            axisLine={false}
+                            tickFormatter={(value) => value.slice(0, 3)}
+                        />
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <Bar dataKey="patients" fill={chartConfig.desktop.color} radius={8}>
+                            <LabelList
+                                position="top"
+                                offset={12}
+                                className="fill-foreground"
+                                fontSize={12}
+                            />
+                        </Bar>
+                    </BarChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    )
+}
--- medicine-frontend/src/medecin/templates/MedecinDashboard.tsx
+++ medicine-frontend/src/medecin/templates/MedecinDashboard.tsx
@@ -2,19 +2,26 @@ import React from "react";
 import {DashboardWrapper} from "@/components/features/layout/DashboardWrapper.tsx";
 import PatientsCard from "@/medecin/components/cards/PatientsCard.tsx";
 import AppointmentsCard from "@/medecin/components/cards/AppointmentsCard.tsx";
+import {PatientsAreaChart} from "@/medecin/components/charts/PatientsAreaChart.tsx";
 
 
 const MedecinDashboard = () => {
     return (
         <DashboardWrapper>
-            <div className={"mt-12"}>
+            <div className={"mt-12 w-full"}>
                 <div className={"flex flex-col text-2xl text-black"}>
                     <h2 className={"font-semibold "}>Tableau de Bord</h2>
                     <p className={"font-medium"}>Vos statistiques globales de vos données.</p>
                 </div>
                 <div className={"flex flex-row mt-5"}>
-                    <PatientsCard />
-                    <AppointmentsCard />
+                    <PatientsCard/>
+                    <AppointmentsCard/>
+                </div>
+
+                <div className={"flex w-full items-center mt-3"}>
+                    <div className={"w-[965px] h-[300px]"}>
+                        <PatientsAreaChart/>
+                    </div>
                 </div>
             </div>
         </DashboardWrapper>
