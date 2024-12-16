0a67cb72a598de0c71dba4dfcc31a94c264bb336
Lilian CAFFIER <krykox@Mac.lan>
2024-12-16 20:21:06 +0100

    Replace Appointments & Patients Card to a Global card component :star:

---
+++ medicine-frontend/src/medecin/components/cards/StatsCard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Separator } from "@/components/ui/separator.tsx";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card.tsx";
+import { LucideIcon } from "lucide-react";
+import { Link } from "react-router";
+
+export type StatCardProps = {
+    icon: LucideIcon;
+    title: string;
+    value: number | string;
+    percentage?: string;
+    linkText: string;
+    linkTo: string;
+};
+
+const StatCard = ({icon: Icon, title, value, percentage, linkText, linkTo}: StatCardProps) => {
+    return (
+        <Card className="w-1/2 mx-auto mr-0 md:mr-6 border-2 border-gray-200">
+            <CardHeader>
+                <CardTitle className="text-right w-full text-sm">{percentage}%</CardTitle>
+            </CardHeader>
+            
+            <CardContent className="py-6 flex flex-col sm:flex-row items-center px-6 sm:px-12 -mt-4">
+                <Icon size={50} className="text-gray-400 p-1 border-2 border-gray-200 rounded-lg" />
+                <div className="flex flex-col mt-4 sm:mt-0 sm:ml-8 md:ml-16">
+                    <h2 className="text-gray-400 text-xl sm:text-2xl">{title}</h2>
+                    <span className="text-xl sm:text-2xl text-black font-bold">{value}</span>
+                </div>
+            </CardContent>
+            
+            <Separator className="border bg-gray-200" />
+            
+            <CardFooter className="bg-[#FBFBFB] py-4 rounded-b-lg">
+                <div className="flex flex-row justify-between sm:justify-start items-center">
+                    <Link to={linkTo} className="text-red-800 text-sm">
+                        {linkText}
+                    </Link>
+                    <Icon size={12} className="text-red-800 ml-4" />
+                </div>
+            </CardFooter>
+        </Card>
+    );
+};
+
+export default StatCard;
--- medicine-frontend/src/medecin/templates/MedecinDashboard.tsx
+++ medicine-frontend/src/medecin/templates/MedecinDashboard.tsx
@@ -1,11 +1,30 @@
-import PatientsCard from "@/medecin/components/cards/PatientsCard.tsx";
+import StatCard, {StatCardProps} from "@/medecin/components/cards/StatsCard.tsx";
+import {CalendarCheck, Users} from "lucide-react";
 import React from "react";
 import {DashboardWrapper} from "@/components/features/layout/DashboardWrapper.tsx";
-import AppointmentsCard from "@/medecin/components/cards/AppointmentsCard.tsx";
 import {PatientsAreaChart} from "@/medecin/components/charts/PatientsAreaChart.tsx";
 
 
 const MedecinDashboard = () => {
+    const statsCards: StatCardProps[] = [
+        {
+            icon: Users,
+            title: "Vos Patients",
+            value: 312,
+            percentage: "+10",
+            linkText: "Voir les détails",
+            linkTo: "/patients"
+        },
+        {
+            icon: CalendarCheck,
+            title: "Rendez-Vous",
+            value: 3,
+            percentage: "+10",
+            linkText: "Voir les détails",
+            linkTo: "/appointments"
+        },
+    ]
+    
     return (
         <DashboardWrapper>
             <div className={"mt-12 w-full"}>
@@ -14,8 +33,16 @@ const MedecinDashboard = () => {
                     <p className={"font-medium"}>Vos statistiques globales de vos données.</p>
                 </div>
                 <div className={"flex flex-row mt-5"}>
-                    <PatientsCard/>
-                    <AppointmentsCard />
+                    {statsCards.map((item) => {
+                        return (<StatCard
+                            icon={item.icon}
+                            title={item.title}
+                            value={item.value}
+                            percentage={item.percentage}
+                            linkText={item.linkText}
+                            linkTo={item.linkTo}
+                        />)
+                    })}
                 </div>
 
                 <div className={"flex w-full items-center mt-3"}>
