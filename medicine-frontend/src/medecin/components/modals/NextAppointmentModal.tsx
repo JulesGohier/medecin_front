3394fef15f983ba5abd17a345bb49b3bc6f4db1d
Lilian CAFFIER <krykox@Mac.lan>
2024-12-17 07:24:04 +0100

    Implementation of Transitions store :repeat:

--- medicine-frontend/src/medecin/components/modals/NextAppointmentModal.tsx
+++ medicine-frontend/src/medecin/components/modals/NextAppointmentModal.tsx
@@ -1,6 +1,8 @@
 import {Button} from "@/components/ui/button.tsx";
 import {Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle} from "@/components/ui/dialog.tsx";
 import {PatientsAppointment} from "@/medecin/components/cards/NextAppointmentCard.tsx";
+import {LoaderSpinner} from "@/medecin/components/LoaderSpinner.tsx";
+import {useTransitionStore} from "@/medecin/stores/TransitionStore.tsx";
 import {DialogTrigger} from "@radix-ui/react-dialog";
 import {Calendar, Mail} from "lucide-react";
 import {useState} from "react";
@@ -8,6 +10,7 @@ import {Link} from "react-router";
 
 export const NextAppointmentModal = ({ firstName, lastName, email, day, hours }: PatientsAppointment) => {
     const [isOpen, setIsOpen] = useState(false);
+    const { isPending, startTransition } = useTransitionStore();
     
     const handleClick = () => {
         setIsOpen(false);
@@ -33,9 +36,14 @@ export const NextAppointmentModal = ({ firstName, lastName, email, day, hours }:
                     <p>Contacter le patient : <Link to={`mailto:${email}`} className={"underline text-red-800 hover:text-red-700"}>{email}</Link></p>
                 </div>
                 <DialogFooter>
-                    <Button type="submit" onClick={handleClick}
-                            className={"bg-red-800 hover:bg-red-700 w-full flex items-center gap-2"}>
-                        Quitter
+                    <Button
+                        type="submit"
+                        className={"bg-red-800 hover:bg-red-700 w-full flex items-center gap-2"}
+                        onClick={() => {
+                            startTransition(handleClick)
+                        }}
+                    >
+                        {isPending ? <LoaderSpinner /> : <>Quitter</>}
                     </Button>
                 </DialogFooter>
             </DialogContent>
