6fd49affb3a63323a8ea297555ef3ca38ac1d1c0
JulesGohier <jules.reihog5@gmail.com>
2024-12-18 10:17:09 +0100

    add toast
    add a toast to confirm that the appointment has been made

--- medicine-frontend/src/patient/components/SelectAppointment.tsx
+++ medicine-frontend/src/patient/components/SelectAppointment.tsx
@@ -8,18 +8,27 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog";
 import {useState} from "react";
+import { useToast } from "@/hooks/use-toast.ts";
+import {Toaster} from "@/components/ui/toaster.tsx";
 
 export const SelectAppointment = ({heure, date}: {heure: string, date: string})=> {
     const [isOpen, setIsOpen] = useState(false);
+    const { toast } = useToast();
+
     const handClick = () =>{
         console.log(`RDV le ${date} à ${heure}`);
-        setIsOpen(false)
+        setIsOpen(false);
+
+        toast({
+            title: "Rendez-vous confirmé",
+            description: `Votre rendez-vous pour le ${date} à ${heure} a été confirmé.`
+        });
     }
 
     return (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogTrigger asChild>
-                <Button variant="outline" className={"bg-red-500 hover:bg-red-600 hover:text-white text-white"}> {heure}</Button>
+                <Button variant="outline" className={"bg-red-500 hover:bg-red-600 hover:text-white text-white w-1/5"}> {heure}</Button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
@@ -29,11 +38,12 @@ export const SelectAppointment = ({heure, date}: {heure: string, date: string})=
                     <p>Votre rendez-vous est programmé pour le <br /> {date} à {heure}</p>
                 </div>
                 <DialogFooter>
-                    <Button type="submit" onClick={handClick} className={"bg-red-500 hover:bg-red-600 w-full flex items-center gap-2"}>
+                    <Button onClick={handClick} className={"bg-red-500 hover:bg-red-600 w-full flex items-center gap-2"}>
                         Confirmer le RDV
                     </Button>
                 </DialogFooter>
             </DialogContent>
+            <Toaster />
         </Dialog>
     );
 }
